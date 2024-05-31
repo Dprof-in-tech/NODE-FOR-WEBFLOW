9538cfb5e7b6fda9a079611e1c3371b9a96e313c
Isaac Onyemaechi <amaechiisaac450@gmail.com>
2024-05-31 07:14:36 +0100

    fixes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 // Endpoint to serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public','index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Endpoint to create a payment intent
